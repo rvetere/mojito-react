0a3ff70326eaf9a35e49223ae07724adc8dcfacc
Vetere Remo <Remo.Vetere@axelspringer.ch>
2016-01-17 01:50:20 +0100

    Improvements

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,4 +1,5 @@
 import uuid from 'node-uuid';
+import AltContainer from 'alt-container';
 import React from 'react';
 import Note from './Note.jsx';
 import Notes from './Notes.jsx';
@@ -6,25 +7,6 @@ import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = NoteStore.getState();
-	}
-
-	componentDidMount() {
-		NoteStore.listen(this.storeChanged);
-	}
-
-	componentWillUnmount() {
-		NoteStore.unlisten(this.storeChanged);
-	}
-
-	storeChanged = (state) => {
-		// Without a property initializer `this` wouldn't
-		// point at the right context (defaults to `undefined` in strict mode).
-		this.setState(state);
-	};
-
 	render() {
 		const notes = this.state.notes;
 
@@ -32,9 +14,14 @@ export default class App extends React.Component {
 		return (
 			<div>
 				<button className="add-note" onClick={this.addNote}>+</button>
-				<Notes notes={notes}
-					   onEdit={this.editNote}
-					   onDelete={this.deleteNote}/>
+				<AltContainer
+					stores={[NoteStore]}
+					inject={{
+						notes: () => NoteStore.getState().notes
+					}}
+					>
+					<Notes onEdit={this.editNote} onDelete={this.deleteNote}/>
+				</AltContainer>
 			</div>
 		);
 	}
